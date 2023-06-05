c0b9ede3cf2d02b0db00cc7e6b2f52b4d79b0057
Vladislav Yakovlev <normsvlad@yandex.ru>
2023-06-05 19:50:28 +0500

    init

---
+++ src/components/Todo.jsx
@@ -0,0 +1,51 @@
+import React, { useState } from 'react';
+import TodoForm from './TodoForm';
+import { RiCloseCircleLine } from 'react-icons/ri';
+import { TiEdit } from 'react-icons/ti';
+
+function Todo({ todos, completeTodo, removeTodo, updateTodo }) {
+  const [edit, setEdit] = useState({
+    id: null,
+    value: '',
+  });
+
+  const submitUpdate = (value) => {
+    updateTodo(edit.id, value);
+    setEdit({
+      id: null,
+      value: '',
+    });
+  };
+
+  if (edit.id) {
+    return <TodoForm edit={edit} onSubmit={submitUpdate} />;
+  }
+
+  return todos.map((todo, index) => (
+    <div
+      className={todo.isComplete ? 'todo-row complete' : 'todo-row'}
+      key={index}
+      onClick={() => completeTodo(todo.id)}
+    >
+      <div key={todo.id}>{todo.text}</div>
+      <div className="icons">
+        <RiCloseCircleLine
+          onClick={(e) => {
+            e.stopPropagation();
+            removeTodo(todo.id);
+          }}
+          className="delete-icon"
+        />
+        <TiEdit
+          onClick={(e) => {
+            e.stopPropagation();
+            setEdit({ id: todo.id, value: todo.text });
+          }}
+          className="edit-icon"
+        />
+      </div>
+    </div>
+  ));
+}
+
+export default Todo;
